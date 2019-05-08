396db9d8d245336fc181c38fcf1233c34c74875e
Ahmad <ahmad.hussain.307@gmail.com>
2019-05-08 03:30:35 +0200

    Added static files

--- AirAssignment/AirAssignment/urls.py
+++ AirAssignment/AirAssignment/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from users import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.login),
     url(r'^users/', include('users.urls')),
 ]
